cbd79ed11f19e63f651f8e1f3e5dd2e3c07cf24b
EC2 Default User <ec2-user@ip-172-31-40-88.us-west-2.compute.internal>
2016-04-14 01:53:05 +0000

    moved user-specific db methods to userDb.js

--- dbs/baseDb.js
+++ dbs/baseDb.js
@@ -23,63 +23,6 @@ class BaseDb {
         return deferred.promise;
     }
     
-    get(email) {
-        let deferred = Promise.defer();
-
-        this.collection.find({ email: email }, (err, record) => {
-            if (err) {
-                deferred.reject("Failed to get from " + collectionName);
-            }
-
-            //Call factory method here
-
-            deferred.resolve(record);
-        });
-
-        return deferred.promise;
-    }
-
-    update(email, reqRecord) {
-        let deferred = Promise.defer();
-
-        this.collection.update({ email: email }, {$set: reqRecord}, (err, record) => {
-            if (err) {
-                deferred.reject("Failed to update in " + collectionName);
-            }
-
-            //Call factory method here
-
-            deferred.resolve(record);
-        });
-
-        return deferred.promise;
-    }
-
-    insert(reqRecord) {
-        let deferred = Promise.defer();
-
-        this.collection.find({ email: email }, (err, record) => {
-            if (record) {
-                deferred.reject("Identifier is not unique in " + collectionName);
-            }
-            else if (err) {
-                deferred.reject("Failed to get from " + collectionName);
-            }
-        });
-
-        this.collection.insert(reqRecord, (err, record) => {
-            if (err) {
-                deferred.reject("Failed to insert to " + collectionName);
-            }
-
-            //Call factory method here
-
-            deferred.resolve(record);
-        });
-
-        return deferred.promise;
-    }
-
     setDb(db) {
         this.db = db;
         this.collection = db.collection(this.collectionName);
